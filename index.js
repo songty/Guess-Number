ae1da27734c4b2671d3f02097a42988779c66f40
Tian Song <tian.y.song@gmail.com>
2014-05-16 14:13:47 -0700

    Guesses one fixed number

--- index.js
+++ index.js
@@ -20,3 +20,7 @@ module.exports.multi = function(x, y) {
 	return x * y;
 };
 
+module.exports.compares = function (x) {
+	return x;
+};
+
--- test/test.js
+++ test/test.js
@@ -3,25 +3,32 @@ var expect = chai.expect;
 
 var _ = require('../index');
 
-describe('add', function() {
-	it('adds', function () {
-		expect(_.add(1,5)).to.eql(6);
-	});
-});
+// describe('add', function() {
+// 	it('adds', function () {
+// 		expect(_.add(1,5)).to.eql(6);
+// 	});
+// });
 
-describe('subtract', function() {
-	it('adds', function () {
-		expect(_.subtract(10,5)).to.eql(5);
-	});
-});
+// describe('subtract', function() {
+// 	it('adds', function () {
+// 		expect(_.subtract(10,5)).to.eql(5);
+// 	});
+// });
 
-describe('divide', function() {
-	it('adds', function () {
-		expect(_.divide(200,5)).to.eql(40);
-	});
-});
-describe('multiply', function() {
-	it('multiply', function () {
-		expect(_.multiply(5,5)).to.eql(25);
+// describe('divide', function() {
+// 	it('adds', function () {
+// 		expect(_.div(200,5)).to.eql(40);
+// 	});
+// });
+// describe('multiply', function() {
+// 	it('multiply', function () {
+// 		expect(_.multi(5,5)).to.eql(25);
+// 	});
+// });
+
+//Testing for number > or <
+describe('Greater than', function() {
+	it('compares', function () {
+		expect(_.compares(20)).to.be.above(5);
 	});
 });
